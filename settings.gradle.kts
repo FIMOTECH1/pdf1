ba49c1fa0910b4b927061b6c7a24e7104faff82d
FIMOTECH1 <finiaseliakimu913@gmail.com>
2026-06-07 21:38:45 +0300

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,27 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+
+        //  REQUIRED FOR UCROP (FIX ADDED)
+        maven { url = uri("https://jitpack.io") }
+    }
+}
+
+rootProject.name = "Image To Pdf"
+include(":app")
